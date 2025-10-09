ee56df8cdffde82995641b82925720f74dfcad42
codewithsaidul <codewithsaidul@gmail.com>
2025-10-10 01:24:18 +0600

    project cloning complete

---
+++ src/config/env.ts
@@ -0,0 +1,75 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+interface ENVCONFIG {
+  PORT: string;
+  // DATABASE_URL: string;
+  NODE_ENV: "development" | "production";
+  // JWT: {
+  //   JWT_ACCESS_SECRET: string;
+  //   JWT_ACCESS_EXPIRATION_TIME: string;
+  //   JWT_REFRESH_SECRET: string;
+  //   JWT_REFRESH_EXPIRATION_TIME: string;
+  // };
+
+  // CLOUDINARY: {
+  //   CLOUDINARY_CLOUD_NAME: string;
+  //   CLOUDINARY_API_KEY: string;
+  //   CLOUDINARY_API_SECRET: string;
+  // };
+
+  // BCRYPT_SALT_ROUND: string;
+  // ADMIN_EMAIL: string;
+  // ADMIN_PASSWORD: string;
+}
+
+const loadEnvVariable = (): ENVCONFIG => {
+  const requiredEnvVariables: string[] = [
+    "PORT",
+    // "DATABASE_URL",
+    "NODE_ENV",
+
+    // "JWT_ACCESS_SECRET",
+    // "JWT_ACCESS_EXPIRATION_TIME",
+    // "JWT_REFRESH_SECRET",
+    // "JWT_REFRESH_EXPIRATION_TIME",
+
+    // "CLOUDINARY_CLOUD_NAME",
+    // "CLOUDINARY_API_KEY",
+    // "CLOUDINARY_API_SECRET",
+
+    // "BCRYPT_SALT_ROUND",
+    // "ADMIN_EMAIL",
+    // "ADMIN_PASSWORD",
+  ];
+
+  requiredEnvVariables.forEach((key) => {
+    if (!process.env[key]) {
+      throw new Error(`Missing require environment variable ${key}`);
+    }
+  });
+
+  return {
+    PORT: process.env.PORT as string,
+    // DATABASE_URL: process.env.DB_URL as string,
+    NODE_ENV: process.env.NODE_ENV as "development" | "production",
+    // JWT: {
+    //   JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET as string,
+    //   JWT_ACCESS_EXPIRATION_TIME: process.env
+    //     .JWT_ACCESS_EXPIRATION_TIME as string,
+    //   JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET as string,
+    //   JWT_REFRESH_EXPIRATION_TIME: process.env
+    //     .JWT_REFRESH_EXPIRATION_TIME as string,
+    // },
+    // CLOUDINARY: {
+    //   CLOUDINARY_CLOUD_NAME: process.env.CLOUDINARY_CLOUD_NAME as string,
+    //   CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY as string,
+    //   CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET as string,
+    // },
+    // BCRYPT_SALT_ROUND: process.env.BCRYPT_SALT_ROUND as string,
+    // ADMIN_EMAIL: process.env.ADMIN_EMAIL as string,
+    // ADMIN_PASSWORD: process.env.ADMIN_PASSWORD as string,
+  };
+};
+
+export const envVars = loadEnvVariable();
